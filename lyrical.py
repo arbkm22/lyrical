00de8d38f04e125cf17ed2b53b79893c48ce2b1e
Bhaskar Mahto <arbkm22@gmail.com>
2024-04-21 18:54:07 +0530

    - added user input - handle search query - added .gitignore and .env

--- lyrical.py
+++ lyrical.py
@@ -1,21 +1,38 @@
+import os
+from dotenv import load_dotenv
 import dearpygui.dearpygui as dpg
+import requests
 
-def save_callback():
-    print("Save clicked")
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+
+def searchSong():
+    searchQuery = dpg.get_value("search_box")
+    base_url = "https://api.genius.com"
+    headers = {'Authorization': 'Bearer ' + TOKEN}
+    search_url = base_url + "/search"
+    data = {'q': searchQuery}
+    response = requests.get(search_url, data=data, headers=headers)
+    json = response.json()
+
+    print(f'json response: {json}')
 
 def main():
     dpg.create_context()
     dpg.create_viewport(height=800, width=600)
-
     dpg.setup_dearpygui()
 
-    with dpg.window(label="Lyrical"):
+    with dpg.window(tag="Lyrical"):
         dpg.add_text("Hello, lyrical")
-        dpg.add_button(label="Save", callback=save_callback)
-        dpg.add_input_text(label="String")
-        dpg.add_slider_float(label="Float")
+        dpg.add_text("Search for a song")
+        # dpg.add_button(label="Save", callback=save_callback)
+        dpg.add_input_text(tag="search_box")
+        # dpg.add_slider_float(label="Float")
+        dpg.add_button(label="Search", tag="search-btn", callback=searchSong)
 
     dpg.show_viewport()
+    dpg.set_primary_window("Lyrical", True)
     dpg.start_dearpygui()
     dpg.destroy_context()
 
