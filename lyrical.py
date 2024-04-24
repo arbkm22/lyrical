31fd02251d873e439952cd2cc50a4e26d01d84b2
Bhaskar Mahto <arbkm22@gmail.com>
2024-04-24 23:55:43 +0530

    looking for other options for lyrics api

--- lyrical.py
+++ lyrical.py
@@ -16,10 +16,10 @@ def make_api_callout(search_query):
     headers = {'Authorization': 'Bearer ' + TOKEN}
     search_url = base_url + "/search"
     data = {'q': search_query}
-    response = requests.get(search_url, data=data, headers=headers)
+    response = requests.get(search_url, data=data, headers=headers, timeout=(2, 4))
+    print(response.text)
     json = response.json()
-    time.sleep(2)
-    print(f'json in function: {json}')
+    # print(f'json in function: {json}')
     return json;
 
 def searchSong():
@@ -33,11 +33,8 @@ def searchSong():
     
     
     # print(json)
-    json_val = threading.Thread(target=make_api_callout, args=(search_query, ), daemon=True)
-    json_val.start()
-    json_val.join()
-    json_data = json_val
-    print(json_data)
+    json_val = make_api_callout(search_query)
+    # print(json_val)
     # if (json_data['meta']['status'] == 200):
     #     print('status code 200: ', json_data)
     #     response = json_data['response']
